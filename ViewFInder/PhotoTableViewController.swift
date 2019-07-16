0c10a493c32298fe68ff75013ffa79c1dfe58e1d
Apple <apple@Apples-MBP-4.attlocal.net>
2019-07-16 11:38:17 -0700

    done bros

--- ViewFInder/PhotoTableViewController.swift
+++ ViewFInder/PhotoTableViewController.swift
@@ -63,8 +63,20 @@ class PhotoTableViewController: UITableViewController {
 
         return cell
     }
-    
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveToDetail", sender: photos[indexPath.row])
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "moveToDetail" {
+            if let photoDetailView = segue.destination as? PhotoDetailViewController {
+                
+                if let photoToSend = sender as? Photos {
+                    photoDetailView.photo = photoToSend
+                }
+                
+            }
+        }
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -73,20 +85,24 @@ class PhotoTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
+                let photoToDelete = photos[indexPath.row]
+                context.delete(photoToDelete)
+                
+                (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                getPhotos() 
+            }
+        }
     }
-    */
+    
 
     /*
-    // Override to support rearranging the table view.
+    // Override to supportrearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
